b4a52887003a9be5d53a36b19eadf85e7c88dbe6
Jan Grodowski <jgrodowski@gmail.com>
2014-12-20 18:12:54 +0100

    update specs and requires

--- solution.rb
+++ solution.rb
@@ -1,7 +1,7 @@
 # NewRelic Metaprogramming Challange 2014
 # Author: Jan Grodowski, jgrodowski@gmail.com, http://github.com/grodowski
 
-require './lib/method_call_counter'
+require_relative 'lib/method_call_counter'
 
 MethodCallCounter.start! ENV['COUNT_CALLS_TO']
 at_exit { puts MethodCallCounter.status }
--- test/method_call_counter_test.rb
+++ test/method_call_counter_test.rb
@@ -2,9 +2,6 @@ require 'minitest/autorun'
 require './lib/method_call_counter'
 
 class SolutionTest < MiniTest::Test
-  def test_empty_args
-    
-  end
   
   def test_instance_method
     MethodCallCounter.start! 'String#size'
@@ -14,40 +11,44 @@ class SolutionTest < MiniTest::Test
   
   def test_class_method
     MethodCallCounter.start! 'Math.sin'
-    4.times { |n| Math.sin(n + 0.33) }
+    eval "4.times { |n| Math.sin(n + 0.33) }"
     assert_call_count 4
   end
   
   def test_included_module
-    MethodCallCounter.start! 'B#foo'
-    # TODO
-    # module A
-    #   def foo
-    #   end
-    # end
-    #
-    # class B
-    #   include A
-    # end
-    # 10.times{B.new.foo}
-    #
-    #assert_call_count 10
+    MethodCallCounter.start! 'SolutionTest::B#foo'
+    eval "module A; def foo; 'test'; end; end; class B; include A; end; 10.times { B.new.foo }"
+    assert_call_count 10
   end
   
   def test_extended_module
-    
+    MethodCallCounter.start! 'SolutionTest::B.foo'
+    eval 'module A; def foo; end; end; class B; extend A; end; 15.times{B.foo}'
+    assert_call_count 15
   end
   
   def test_defined_instance_method
-    
+    MethodCallCounter.start! 'SolutionTest::Foo#bar'
+    eval 'class Foo; def bar(str); str; end; end; 5.times { Foo.new.bar("Placki") }'
+    assert_call_count 5
   end
   
   def test_defined_class_method
-    
+    MethodCallCounter.start! 'SolutionTest::Bar.bar'
+    eval 'class Bar; def self.bar(str); str; end; end; 5.times { Bar.bar "Placki" }'
+    assert_call_count 5
+  end
+  
+  def test_dynamic_class_method
+    MethodCallCounter.start! 'SolutionTest::Baz.bar'
+    eval 'class Baz; end; a = Baz.new; Baz.class_eval do; define_singleton_method :bar do; "a"; end; end; 2.times { Baz.bar }'
+    assert_call_count 2
   end
   
   def test_dynamic_instance_method
-    
+    MethodCallCounter.start! 'SolutionTest::Fooo#bar'
+    eval 'class Fooo; end; a = Fooo.new; Fooo.class_eval do; define_method :bar do; "a"; end; end; 2.times { a.bar }'
+    assert_call_count 2
   end
   
   private
